ec8e9d639ba8574aaa87ee3c689dab786f4a80c3
sokkanji <sskkanji@gmail.com>
2020-04-30 15:22:51 +0900

    댓글 모델 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,32 @@
-from django.urls import path 
+from django.urls import path
 from . import views
+
+# urlpatterns = [
+#     path('', views.post_list, name='post_list'),
+#     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+#     path('post/new/', views.post_new, name='post_new'),
+#     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+#     path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+#     path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+#     path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+
+# ]
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-    path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-    path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post,
+         name='add_comment_to_post'),
+    #     url(r'^comment/(?P<pk>\d+)/approve/$',
+    #         views.comment_approve, name='comment_approve'),
+    #     url(r'^comment/(?P<pk>\d+)/remove/$',
+    #         views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/approve/',
+         views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
